1eb066f5b81dd5ef1e684f7275d219942af3541a
Alexander Pitkin <Alexander@peleccom-822292>
2012-07-02 21:52:27 +0300

    Add _PATH to setting.py

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -1,7 +1,13 @@
 # Django settings for gallery project.
+import os
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
