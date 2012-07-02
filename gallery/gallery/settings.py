b099f29d3d32be03bdbf0f216dcd3be489c379be
Alexander Pitkin <C:\Documents and Settings\Alexander\Application Data\The Bat!>
2012-07-02 22:21:36 +0300

    replace absolute paths to relative paths in settings.py

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -54,12 +54,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = r'd:/Sashaprg/py/djcode/gallery/media/'
+MEDIA_ROOT = os.path.join(_PATH,'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
