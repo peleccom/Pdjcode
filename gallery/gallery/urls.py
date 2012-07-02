76b344e529269568a05e63cb02501bbea48240c6
Alexander Pitkin <Alexander@peleccom-822292>
2012-07-03 01:54:53 +0300

    Proper serving static and media in urls.py

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,5 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
 url(r'^admin/', include(admin.site.urls)),
-(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+#(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
